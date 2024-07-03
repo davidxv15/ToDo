ac13f26ac95c6ddd450e5e566cf756a809d11b51
davidxv15 <davidxvaz@gmail.com>
2024-07-03 13:11:57 -0700

    TodoList.jsx

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,16 @@
+// Import statements
+import React from 'react';
+import TodoItem from './TodoItem';
+
+// Functional component
+const TodoList = ({ todos, removeTodo }) => {
+  return (
+    <ul>
+      {todos.map((todo, index) => (
+        <TodoItem key={index} index={index} todo={todo} removeTodo={removeTodo} />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
