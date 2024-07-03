8bed409ab720592347618fafc137a47715c4596b
davidxv15 <davidxvaz@gmail.com>
2024-07-03 13:13:34 -0700

    Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+// Import statements
+import React from 'react';
+
+// Functional component
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2024 Todo App</p>
+    </footer>
+  );
+};
+
+export default Footer;
